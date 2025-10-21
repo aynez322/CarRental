fe48afe88c8adb29103840828da4e17650f5cb20
aynez322 <jalbasenea12@gmail.com>
2025-10-21 15:16:40 +0300

    use case + document

--- CarRental-frontend/src/main.jsx
+++ CarRental-frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AvailableCars } from './pages/AvailableCars/AvailableCars.jsx'
+import { HomePage } from './pages/Home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AvailableCars/>
     {/* <App/> */}
+    {/* <HomePage/> */}
   </StrictMode>,
 )
---
+++ CarRental-frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+export function HomePage(){
+    return (
+        <>
+            <div className="homePage">
+                <>
+                    <h1 className="wellcomeheader">Wellcome to Car Rental</h1>
+                </>
+            </div>
+        </>
+    );
+}
