b8b2fcdf414280b4fab2dca54e6928295807a08c
aynez322 <jalbasenea12@gmail.com>
2025-11-07 14:23:31 +0200

    am adaugat link la butoanele din navbar

--- CarRental-frontend/src/components/common/NavBar/NavBar.jsx
+++ CarRental-frontend/src/components/common/NavBar/NavBar.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 import logo from '../../../assets/logo.png';
 
 export default function NavBar() {
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <header className="nav">
       <div className="nav__container">
         <div className="nav__brand">
-         <a href="/" className="nav__brand-link">
-          <img src={logo} alt="Car Rental Logo" className="nav__logo" />
-         </a>
+          <Link to="/" className="nav__brand-link">
+            <img src={logo} alt="Car Rental Logo" className="nav__logo" />
+          </Link>
         </div>
 
         <nav className="nav__links">
-          <a href="/" className="nav__link">Home</a>
-          <a href="#cars" className="nav__link">Cars</a>
-          <a href="/bookings" className="nav__link">My Bookings</a>
-          <a href="#contact" className="nav__link">Contact</a>
+          <Link to="/" className="nav__link">Home</Link>
+          <Link to="/cars" className="nav__link">Cars</Link>
+          <Link to="/bookings" className="nav__link">My Bookings</Link>
+          
+          <button 
+            onClick={() => scrollToSection('contact')} 
+            className="nav__link"
+          >
+            Contact
+          </button>
+          
           <button className="nav__login">Login</button>
-          <button className="nav__admin">Admin</button>
+          
+          <Link to="/admin" className="nav__admin">Admin</Link>
         </nav>
       </div>
     </header>
