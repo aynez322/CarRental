bbd3280b02007ff112f15f49e65d47ca05186d60
aynez322 <jalbasenea12@gmail.com>
2025-11-07 14:16:06 +0200

    am adaugat route

--- CarRental-frontend/src/App.jsx
+++ CarRental-frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import AdminPage from './pages/AdminPage/AdminPage';
 import './App.css';
+import MyBookings from './pages/MyBookings/MyBookings';
+import { AvailableCars } from './pages/AvailableCars/AvailableCars';
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/bookings" element={<MyBookings/>} />
+      <Route path="/cars" element={<AvailableCars/>}/>
+    </Routes>
+  );
 }
 
 export default App;
