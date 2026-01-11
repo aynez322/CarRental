3cda2015ba7e98d4f87ecbdca10f88e512e42b9a
aynez322 <jalbasenea12@gmail.com>
2026-01-11 14:47:16 +0200

    deleted sql migration files

--- db_migration_20260111_booking_idnp_license.sql
+++
@@ -1,9 +0,0 @@
--- Adds IDNP + driver license image URLs to bookings
-ALTER TABLE bookings
-  ADD COLUMN IF NOT EXISTS idnp VARCHAR(13);
-
-ALTER TABLE bookings
-  ADD COLUMN IF NOT EXISTS driver_license_front_url VARCHAR(255);
-
-ALTER TABLE bookings
-  ADD COLUMN IF NOT EXISTS driver_license_back_url VARCHAR(255);
